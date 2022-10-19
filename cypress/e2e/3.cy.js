da5c950ca4e77471767583c6910b800f5f34cebf
HuynhHuongNguyen <huong.nh@drd-x.com>
2022-10-19 15:21:00 +0700

    test1

--- cypress/e2e/3.cy.js
+++ cypress/e2e/3.cy.js
@@ -1,5 +1,9 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:8080') // change URL to match your dev URL
+describe('My First Test', function() {
+  it('Search on amazon', function() {
+    cy.viewport(2440,1200)
+    cy.visit('https://www.amazon.com/')
+    cy.get('#twotabsearchtextbox').type('Superman')
+    cy.get('#nav-search-submit-button').click()
+    // cy.get('span.a-color-state').contains('Superman')
   })
 })
